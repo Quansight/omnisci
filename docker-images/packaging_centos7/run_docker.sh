aec8d69e09f400f4c375a3d081a7728f8fc9d325
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-05-28 00:49:54 -0400

    Added mapd-core source.

--- docker-images/packaging_centos7/run_docker.sh
+++ docker-images/packaging_centos7/run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -v /home/xmn/.ssh/id_rsa:/id_rsa:ro -v /home/xmn/.gitconfig:/home/dev/.gitconfig:ro  -v ~/dev/quansight/conda-recipes:/home/default/code pkg_mapd_centos7 bash
+docker run -it -v /home/xmn/.ssh/id_rsa:/id_rsa:ro -v /home/xmn/.gitconfig:/home/dev/.gitconfig:ro   pkg_mapd_centos7 bash
