ee8327ffaaee695c1aea9f2bd020eb8122223a74
Saul Shanabrook <s.shanabrook@gmail.com>
2018-06-05 14:22:30 -0400

    Switch to using row id branch

--- notebooks/test.bash
+++ notebooks/test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo
-jupyter nbconvert --execute "Ibis + Altair.ipynb" "Vega Rendering.ipynb"
+jupyter nbconvert --execute "Ibis + Altair.ipynb" "Vega Rendering.ipynb" "Altair Example - Bubble Plot.ipynb"
 # git diff --exit-code
 set +euxo
